41e1b5800641f35d5db8c2f63f83c38cb1d39275
Palash Bag <palashbag903@gmail.com>
2023-05-16 02:05:03 +0530

    first commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spinner() {
+  return (
+    <>
+     <div className='w-screen h-[calc(100vh-80px)] flex justify-center items-center'>
+        <div className="spinner"></div>
+     </div> 
+    </>
+  )
+}
